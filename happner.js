66cffe87e65d51de2c02d5edfee9db3820831d25
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-09-27 12:07:22 +0200

    updated happner.js

--- happner.js
+++ happner.js
@@ -14,3 +14,30 @@ module.exports = {
 		}
 	}
 }
+
+/*
+an alternative - demonstrates how one would share a bunch of modules on a single npm tree
+module.exports = {
+	suite:[
+		{
+			"id":"happner-dashboard",
+			"path":__dirname + '/index.js',
+			"autoload":{
+				"component":{
+					startMethod:"initialize",
+					schema:{
+				      "exclusive":false
+				    },
+				    web:{
+				      routes:{
+				        "static":"app"
+				      }
+				    }
+				}
+			}
+		}
+	]
+	
+}
+
+*/
