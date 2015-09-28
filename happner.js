8d0a252b220e79766c5672d4a75ffb14ec42a102
nomilous <nomilous@gmail.com>
2015-09-28 19:53:43 +0200

    tidy

--- happner.js
+++ happner.js
@@ -18,31 +18,3 @@ module.exports = {
     }]
   }
 }
-
-/*
-
-an alternative - demonstrates how one would share a bunch of modules on a single npm tree
-module.exports = {
-  suite:[
-    {
-      "name":"happner-dashboard",
-      "path":__dirname + '/index.js',
-      "autoload":{
-        "component":{
-          startMethod:"initialize",
-          schema:{
-            "exclusive":false
-          },
-          web:{
-            routes:{
-              "static":"app"
-            }
-          }
-        }
-      }
-    }
-  ]
-
-}
-
-*/
