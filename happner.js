28c0cbfc27fdf3146599eff4879b8b5171e99361
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-09-28 23:17:02 +0200

    bumped version, modified autoloader

--- happner.js
+++ happner.js
@@ -1,20 +1,45 @@
 module.exports = {
   configs: {
-    "autoload": [{
-      "component":{
-        name: "happner-dashboard",
-        config: {
-          startMethod:"initialize",
-          schema:{
-            "exclusive":false
-          },
-          web:{
-            routes:{
-              "static":"app"
+    "autoload": [
+      {
+        "component":{
+          name: "happner-dashboard",
+          config: {
+            startMethod:"initialize",
+            schema:{
+              "exclusive":false
+            },
+            web:{
+              routes:{
+                "static":"app"
+              }
+            }
+          }
+        }
+      },
+      {
+        "module":{
+          name:"happner-resources",
+          config:{
+            path:__dirname + "/node_modules/happner-resources/index.js"
+          }
+        },
+        "component":{
+          name:"happner-resources",
+          moduleName: "happner-resources",
+          config: {
+            startMethod:"initialize",
+            schema:{
+              "exclusive":false
+            },
+            web:{
+              routes:{
+                "static":__dirname + "/node_modules/happner-resources/resources"
+              }
             }
           }
         }
       }
-    }]
+      ]
   }
 }
