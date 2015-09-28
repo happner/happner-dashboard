240e5b2faf576f0ccaf6cca3a83db73f149faf78
nomilous <nomilous@gmail.com>
2015-09-28 18:08:17 +0200

    jiggle format for happner.js

--- happner.js
+++ happner.js
@@ -1,43 +1,48 @@
 module.exports = {
-	"name":"happner-dashboard",
-	"autoload":{
-		"component":{
-			startMethod:"initialize",
-			schema:{
-		      "exclusive":false
-		    },
-		    web:{
-		      routes:{
-		        "static":"app"
-		      }
-		    }
-		}
-	}
+  configs: {
+    "autoload":{
+      "component":{
+        name: "happner-dashboard",
+        config: {
+          startMethod:"initialize",
+          schema:{
+            "exclusive":false
+          },
+          web:{
+            routes:{
+              "static":"app"
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 /*
+
 an alternative - demonstrates how one would share a bunch of modules on a single npm tree
 module.exports = {
-	suite:[
-		{
-			"name":"happner-dashboard",
-			"path":__dirname + '/index.js',
-			"autoload":{
-				"component":{
-					startMethod:"initialize",
-					schema:{
-				      "exclusive":false
-				    },
-				    web:{
-				      routes:{
-				        "static":"app"
-				      }
-				    }
-				}
-			}
-		}
-	]
-	
+  suite:[
+    {
+      "name":"happner-dashboard",
+      "path":__dirname + '/index.js',
+      "autoload":{
+        "component":{
+          startMethod:"initialize",
+          schema:{
+            "exclusive":false
+          },
+          web:{
+            routes:{
+              "static":"app"
+            }
+          }
+        }
+      }
+    }
+  ]
+
 }
 
 */
