6599fc0246cbe3725968c5799d32449e5d98f72a
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2015-10-11 12:00:24 +0200

    bumped version

--- happner.js
+++ happner.js
@@ -16,30 +16,7 @@ module.exports = {
             }
           }
         }
-      },
-      {
-        "module":{
-          name:"happner-resources",
-          config:{
-            path:__dirname + "/node_modules/happner-resources/index.js"
-          }
-        },
-        "component":{
-          name:"happner-resources",
-          moduleName: "happner-resources",
-          config: {
-            startMethod:"initialize",
-            schema:{
-              "exclusive":false
-            },
-            web:{
-              routes:{
-                "static":__dirname + "/node_modules/happner-resources/resources"
-              }
-            }
-          }
-        }
       }
-      ]
+    ]
   }
 }
